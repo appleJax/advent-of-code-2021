8a8f1ae3f1faea69288db0b60bf432a41e830d16
appleJax <appleJax@users.noreply.github.com>
2021-12-12 15:33:16 -0800

    day1 solution

--- src/day1/solution.spec.ts
+++ src/day1/solution.spec.ts
@@ -1,13 +1,45 @@
 import path from "path";
 import fs from "fs";
-import solution from "./solution";
+import { solutionA, solutionB } from "./solution";
 
-const input = fs
+const officialInput = fs
   .readFileSync(path.resolve(__dirname, "input.txt"), "utf-8")
-  .split("\n");
+  .split("\n")
+  .map(Number);
 
-describe("Day 1", () => {
-  test("Sonar Sweep", () => {
-    expect(solution(input)).toBe(true);
+describe("Day 1 - Part 1", () => {
+  test("empty array", () => {
+    expect(solutionA([])).toBe(0);
+  });
+
+  test("no increase", () => {
+    expect(solutionA([5, 4, 3, 2, 2, 1])).toBe(0);
+  });
+
+  test("simple case", () => {
+    expect(solutionA([1, 2, 3, 2, 3, 4, 1])).toBe(4);
+  });
+
+  test("Sonar Sweep - Part 1", () => {
+    expect(solutionA(officialInput)).toBe(1215);
+  });
+});
+
+describe("Day 1 - Part 2", () => {
+  test("not enough data", () => {
+    expect(solutionB([])).toBe(0);
+    expect(solutionB([100, 101, 102])).toBe(0);
+  });
+
+  test("no increase", () => {
+    expect(solutionB([100, 90, 130, 95, 85, 120])).toBe(0);
+  });
+
+  test("simple case", () => {
+    expect(solutionB([100, 90, 130, 95, 95, 120, 100, 97, 70])).toBe(3);
+  });
+
+  test("Sonar Sweep - Part 2", () => {
+    expect(solutionB(officialInput)).toBe(1150);
   });
 });
--- src/day1/solution.ts
+++ src/day1/solution.ts
@@ -1,3 +1,27 @@
-export default function solution(input: string[]) {
-  return input.length > 0;
+export function solutionA(depths: number[]) {
+  let prevDepth = Infinity;
+  return depths.reduce((increases, depth) => {
+    let nextValue = increases;
+    if (depth > prevDepth) {
+      nextValue += 1;
+    }
+    prevDepth = depth;
+
+    return nextValue;
+  }, 0);
+}
+
+export function solutionB(depths: number[]) {
+  if (depths.length < 4) {
+    return 0;
+  }
+
+  let increases = 0;
+  for (let i = 3; i < depths.length; i++) {
+    if (depths[i] > depths[i - 3]) {
+      increases += 1;
+    }
+  }
+
+  return increases;
 }
