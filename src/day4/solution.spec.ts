ab28ae2602279008c1bd24a329c6ef8a3bd33ffc
appleJax <appleJax@users.noreply.github.com>
2021-12-27 17:54:05 -0800

    implement day4 helpers

--- src/day4/solution.spec.ts
+++ src/day4/solution.spec.ts
@@ -1,18 +1,91 @@
 import path from "path";
 import fs from "fs";
-import { solutionA, solutionB } from "./solution";
+import { getScore, getWinningIndex, parseInput, solutionA, solutionB } from "./solution";
 
-const officialInput = fs
-  .readFileSync(path.resolve(__dirname, "input.txt"), "utf-8")
-  .split("\n");
+const officialInput = fs.readFileSync(
+  path.resolve(__dirname, "input.txt"),
+  "utf-8"
+);
+
+const mockBoard = [
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15],
+  [16, 17, 18, 19, 20],
+  [21, 22, 23, 24, 25],
+];
+
+describe("helpers", () => {
+  test("parseInput", () => {
+    const { boards, drawnIndexes, drawnNums } = parseInput(officialInput);
+
+    const firstBoard = boards[0];
+    expect(firstBoard).toHaveLength(5);
+    expect(firstBoard[0]).toEqual([95, 91, 54, 75, 45]);
+
+    expect(drawnNums[drawnNums.length - 1]).toEqual(60);
+    expect(drawnNums.slice(0, 3)).toEqual([31, 50, 68]);
+
+    expect(drawnIndexes[31]).toEqual(0);
+    expect(drawnIndexes[50]).toEqual(1);
+    expect(drawnIndexes[60]).toEqual(99);
+  });
+
+  test("getWinningIndex - row", () => {
+    const drawnIndexes = {
+      1: 10,
+      2: 20,
+      3: 30,
+      4: 40,
+      5: 50,
+    };
+    expect(getWinningIndex(drawnIndexes)(mockBoard)).toEqual(50);
+  });
+
+  test("getWinningIndex - column", () => {
+    const drawnIndexes = {
+      1: 10,
+      6: 60,
+      11: 110,
+      16: 160,
+      21: 210,
+    };
+    expect(getWinningIndex(drawnIndexes)(mockBoard)).toEqual(210);
+  });
+
+  test("getScore", () => {
+    const drawnIndexes = {
+      21: 0,
+      22: 1,
+      23: 2,
+      24: 3,
+      25: 4,
+      10: 5,
+      11: 6,
+    };
+    expect(getScore({
+      board: mockBoard,
+      drawnIndexes,
+      winningIndex: 4,
+    })).toEqual(210 * 25);
+  });
+});
 
 describe("Day 4 - Part 1", () => {
-  test("degenerate case", () => {
-    expect(solutionA([])).toBe(false);
+  xtest("degenerate case", () => {
+    expect(solutionA("")).toBe(0);
   });
 
   xtest("simple case", () => {
-    expect(solutionA([])).toBe();
+    const input = `21,22,23,24,25
+
+ 1  2  3  4  5
+ 6  7  8  9 10
+11 12 13 14 15
+16 17 18 19 20
+21 22 23 24 25`;
+
+    expect(solutionA(input)).toBe(210 * 25);
   });
 
   xtest("Official Solution - Part 1", () => {
@@ -22,15 +95,14 @@ describe("Day 4 - Part 1", () => {
 
 describe("Day 4 - Part 2", () => {
   xtest("degenerate case", () => {
-    expect(solutionB([])).toBe(false);
+    expect(solutionB("")).toBe(false);
   });
 
   xtest("simple case", () => {
-    expect(solutionB([])).toBe(false);
+    expect(solutionB("")).toBe(false);
   });
 
   xtest("Official Solution - Part 2", () => {
     expect(solutionB(officialInput)).toBe(42);
   });
 });
-
--- src/day4/solution.ts
+++ src/day4/solution.ts
@@ -1,8 +1,73 @@
-export function solutionA(input: unknown[]) {
-  return input;
+type DrawnIndexes = Record<number, number>;
+
+export function solutionA(input: string) {
+  const { boards, drawnIndexes, drawnNums } = parseInput(input);
+
+  let minWinningIndex = Infinity;
+
+  const winningIndex = getWinningIndex(drawnIndexes)(boards[0]);
+
+  return 0;
 }
 
-export function solutionB(input: unknown[]) {
-  return input;
+export function solutionB(input: string) {
+  return 0;
 }
 
+export function parseInput(input: string) {
+  const parsed = input.split("\n\n");
+  const drawnNums = parsed.shift()?.split(",").map(Number) as number[];
+  const drawnIndexes = drawnNums.reduce<DrawnIndexes>((hashmap, num, index) => {
+    hashmap[num] = index;
+    return hashmap;
+  }, {});
+
+  const boards = parsed.map((rawBoard) =>
+    rawBoard.split("\n").map((row) => row.trim().split(/\s+/).map(Number))
+  );
+
+  return {
+    boards,
+    drawnIndexes,
+    drawnNums,
+  };
+}
+
+export function getWinningIndex(drawnIndexes: DrawnIndexes) {
+  return (board: number[][]) => {
+    let winningIndex = Infinity;
+
+    for (let i = 0; i < 5; i++) {
+      const isWinningRow = board[i].every((num) => num in drawnIndexes);
+      if (isWinningRow) {
+        const maxIndex = board[i].reduce(
+          (max, num) => (drawnIndexes[num] < max ? max : drawnIndexes[num]),
+          -Infinity
+        );
+        winningIndex = Math.min(maxIndex, winningIndex);
+      }
+
+      const isWinningColumn = board.every((row) => row[i] in drawnIndexes);
+      if (isWinningColumn) {
+        const maxIndex = board.reduce(
+          (max, row) =>
+            drawnIndexes[row[i]] < max ? max : drawnIndexes[row[i]],
+          -Infinity
+        );
+        winningIndex = Math.min(maxIndex, winningIndex);
+      }
+    }
+
+    return winningIndex;
+  };
+}
+
+type GetScoreArgs = {
+  board: number[][];
+  drawnIndexes: DrawnIndexes;
+  winningIndex: number;
+};
+
+export function getScore({ board, drawnIndexes, winningIndex }: GetScoreArgs) {
+  return 42;
+}
