a5588375de7ebb109613d0fa3b66e7b9fec64c1d
appleJax <appleJax@users.noreply.github.com>
2022-10-17 19:42:22 -0700

    day 7 solution

--- src/day7/solution.spec.ts
+++ src/day7/solution.spec.ts
@@ -4,32 +4,46 @@ import { solutionA, solutionB } from "./solution";
 
 const officialInput = fs
   .readFileSync(path.resolve(__dirname, "input.txt"), "utf-8")
-  .split("\n");
+  .split(",")
+  .map(Number);
 
 describe("Day 7 - Part 1", () => {
   test("degenerate case", () => {
-    expect(solutionA([])).toBe(false);
+    expect(solutionA([])).toBe(0);
+    expect(solutionA([12])).toBe(0);
+    expect(solutionA([7, 7, 7])).toBe(0);
   });
 
-  xtest("simple case", () => {
-    expect(solutionA([])).toBe(false);
+  test("simple case", () => {
+    expect(solutionA([1, 2, 5])).toBe(4);
+    expect(solutionA([1, 2, 5, 9])).toBe(11);
+    expect(solutionA([1, 2, 5, 9, 22])).toBe(28);
   });
 
-  xtest("Official Solution - Part 1", () => {
-    expect(solutionA(officialInput)).toBe(false);
+  test("Official Solution - Part 1", () => {
+    expect(solutionA(officialInput)).toBe(339321);
   });
 });
 
 describe("Day 7 - Part 2", () => {
-  xtest("degenerate case", () => {
-    expect(solutionB([])).toBe(false);
+  test("degenerate case", () => {
+    expect(solutionB([])).toBe(0);
+    expect(solutionB([12])).toBe(0);
+    expect(solutionB([7, 7, 7])).toBe(0);
   });
 
-  xtest("simple case", () => {
-    expect(solutionB([])).toBe(false);
+  test("simple case", () => {
+    expect(solutionB([1, 2, 5])).toBe(7);
+    expect(solutionB([1, 2, 5, 9])).toBe(25);
+    expect(solutionB([0, 1, 1, 0, 2, 5, 9])).toBe(42);
+    expect(solutionB([1, 2, 5, 9, 22])).toBe(161);
   });
 
-  xtest("Official Solution - Part 2", () => {
-    expect(solutionB(officialInput)).toBe(false);
+  test("complex case", () => {
+    expect(solutionB([16, 1, 2, 0, 4, 2, 7, 1, 2, 14])).toBe(168);
+  });
+
+  test("Official Solution - Part 2", () => {
+    expect(solutionB(officialInput)).toBe(95476244);
   });
 });
--- src/day7/solution.ts
+++ src/day7/solution.ts
@@ -1,7 +1,46 @@
-export function solutionA(input: unknown[]) {
-  return input;
+export function solutionA(input: number[]) {
+  input.sort((a, b) => a - b);
+  const target = input[Math.floor(input.length / 2)];
+
+  return input.reduce(
+    (totalFuel, position) => totalFuel + Math.abs(position - target),
+    0
+  );
 }
 
-export function solutionB(input: unknown[]) {
-  return input;
+function getFuel(moves: number) {
+  return Array(moves)
+    .fill(undefined)
+    .reduce((sum, _, i) => sum + i + 1, 0);
+}
+
+// needed since 0 is a valid position but is the identity element for addition
+const OFFSET = 1;
+
+export function solutionB(input: number[]) {
+  const uniqueNums = new Set(input).size;
+  if (uniqueNums < 2) {
+    return 0;
+  }
+  const average = input.reduce((sum, x) => sum + x + OFFSET, 0) / input.length;
+  const targetLo = Math.floor(average);
+  const targetHi = Math.ceil(average);
+
+  const left = input.reduce(
+    (totalFuel, position) =>
+      totalFuel + getFuel(Math.abs(position + OFFSET - targetLo)),
+    0
+  );
+
+  if (targetLo === targetHi) {
+    return left;
+  }
+
+  const right = input.reduce(
+    (totalFuel, position) =>
+      totalFuel + getFuel(Math.abs(position + OFFSET - targetHi)),
+    0
+  );
+
+  return Math.min(left, right);
 }
