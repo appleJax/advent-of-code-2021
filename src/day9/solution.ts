27acd8325b5ecfb0c5032a47acb54de0d12153ff
appleJax <appleJax@users.noreply.github.com>
2022-10-21 16:07:24 -0700

    minor refactor

--- src/day9/solution.ts
+++ src/day9/solution.ts
@@ -1,3 +1,5 @@
+type HeightMap = number[][];
+
 const DELTAS = [
   [-1, 0],
   [0, -1],
@@ -5,21 +7,28 @@ const DELTAS = [
   [1, 0],
 ];
 
-function getHeight(heightMap: number[][], y: number, x: number) {
+function isLowPoint(
+  height: number,
+  heightMap: HeightMap,
+  y: number,
+  x: number
+) {
+  return DELTAS.every(
+    ([deltaY, deltaX]) => height < getHeight(heightMap, y + deltaY, x + deltaX)
+  );
+}
+
+function getHeight(heightMap: HeightMap, y: number, x: number) {
   if (y < 0 || y >= heightMap.length || x < 0 || x >= heightMap[0].length) {
     return Infinity;
   }
   return heightMap[y][x];
 }
 
-export function solutionA(heightMap: number[][]) {
+export function solutionA(heightMap: HeightMap) {
   return heightMap.reduce((riskLevelSum, row, y) => {
     row.forEach((height, x) => {
-      const isLowPoint = DELTAS.every(
-        ([deltaY, deltaX]) =>
-          height < getHeight(heightMap, y + deltaY, x + deltaX)
-      );
-      if (isLowPoint) {
+      if (isLowPoint(height, heightMap, y, x)) {
         riskLevelSum += height + 1;
       }
     });
