4efb23435fb0d9fcfb17c344d130380ea2733c85
appleJax <appleJax@users.noreply.github.com>
2022-12-20 19:06:42 -0800

    day10 solution - part 1

--- src/day10/solution.spec.ts
+++ src/day10/solution.spec.ts
@@ -8,15 +8,23 @@ const officialInput = fs
 
 describe("Day 10 - Part 1", () => {
   test("degenerate case", () => {
-    expect(solutionA([])).toBe(false);
+    expect(solutionA([])).toBe(0);
+    expect(solutionA([""])).toBe(0);
+    expect(solutionA(["", ""])).toBe(0);
   });
 
-  xtest("simple case", () => {
-    expect(solutionA([])).toBe(false);
+  test("simple case", () => {
+    expect(solutionA(["()"])).toBe(0);
+    expect(solutionA(["[()<>]"])).toBe(0);
+    expect(solutionA(["[)"])).toBe(3);
+    expect(solutionA(["(]"])).toBe(57);
+    expect(solutionA(["[}"])).toBe(1197);
+    expect(solutionA(["[>"])).toBe(25137);
+    expect(solutionA(["[}", "[}", "[>"])).toBe(1197 + 1197 + 25137);
   });
 
-  xtest("Official Solution - Part 1", () => {
-    expect(solutionA(officialInput)).toBe(false);
+  test("Official Solution - Part 1", () => {
+    expect(solutionA(officialInput)).toBe(294195);
   });
 });
 
--- src/day10/solution.ts
+++ src/day10/solution.ts
@@ -1,7 +1,51 @@
-export function solutionA(input: unknown[]) {
-  return input;
+type Maybe<T> = T | null;
+
+const startChars = ["(", "[", "{", "<"];
+
+type StartChar = "(" | "[" | "{" | "<";
+type EndChar = ")" | "]" | "}" | ">";
+type ChunkChar = StartChar | EndChar;
+
+const PAIR: Record<EndChar, StartChar> = {
+  ")": "(",
+  "]": "[",
+  "}": "{",
+  ">": "<",
+};
+
+const POINTS: Record<EndChar, number> = {
+  ")": 3,
+  "]": 57,
+  "}": 1197,
+  ">": 25137,
+};
+
+function isStartChar(char: string): char is StartChar {
+  return startChars.includes(char);
 }
 
-export function solutionB(input: unknown[]) {
-  return input;
+function findIllegalChar(chunk: ChunkChar[]): Maybe<EndChar> {
+  const stack: string[] = [];
+
+  for (const char of chunk) {
+    if (isStartChar(char)) {
+      stack.push(char);
+    } else if (stack.pop() !== PAIR[char]) {
+      return char;
+    }
+  }
+
+  return null;
+}
+
+export function solutionA(chunks: string[]) {
+  return chunks.reduce((totalPoints, chunk) => {
+    const illegalChar = findIllegalChar(chunk.split("") as ChunkChar[]);
+    const points = illegalChar ? POINTS[illegalChar] : 0;
+    return totalPoints + points;
+  }, 0);
+}
+
+export function solutionB(_input: string[]) {
+  return 0;
 }
