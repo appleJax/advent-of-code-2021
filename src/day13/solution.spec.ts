2860858f062378d93124b19d2d5e5378cb3deb0c
appleJax <appleJax@users.noreply.github.com>
2023-01-06 23:29:44 -0800

    day 13 solution - part 1

--- src/day13/solution.spec.ts
+++ src/day13/solution.spec.ts
@@ -8,15 +8,39 @@ const officialInput = fs
 
 describe("Day 13 - Part 1", () => {
   test("degenerate case", () => {
-    expect(solutionA([])).toBe(false);
+    expect(solutionA([])).toBe(0);
   });
 
-  xtest("simple case", () => {
-    expect(solutionA([])).toBe(false);
+  test("simple case", () => {
+    expect(
+      solutionA([
+        "6,10",
+        "0,14",
+        "9,10",
+        "0,3",
+        "10,4",
+        "4,11",
+        "6,0",
+        "6,12",
+        "4,1",
+        "0,13",
+        "10,12",
+        "3,4",
+        "3,0",
+        "8,4",
+        "1,10",
+        "2,14",
+        "8,10",
+        "9,0",
+        "",
+        "fold along y=7",
+        "fold along x=5",
+      ])
+    ).toBe(17);
   });
 
-  xtest("Official Solution - Part 1", () => {
-    expect(solutionA(officialInput)).toBe(false);
+  test("Official Solution - Part 1", () => {
+    expect(solutionA(officialInput)).toBe(753);
   });
 });
 
--- src/day13/solution.ts
+++ src/day13/solution.ts
@@ -1,7 +1,72 @@
-export function solutionA(input: unknown[]) {
-  return input;
+export function solutionA(input: string[]) {
+  const { dots, folds } = parseInput(input);
+
+  const firstFold = folds[0];
+  const axis = Object.keys(firstFold)[0] as "x" | "y";
+  const lineNum: number = Object.values(firstFold)[0];
+
+  dots.forEach((dot) => {
+    if (dot[axis] > lineNum) {
+      const diff = dot[axis] - lineNum;
+      dot[axis] = lineNum - diff;
+    }
+  });
+
+  return new Set(dots.map((coord) => JSON.stringify(coord))).size;
 }
 
-export function solutionB(input: unknown[]) {
-  return input;
+export function solutionB(_input: string[]) {
+  return 0;
+}
+
+type Coord = {
+  x: number;
+  y: number;
+};
+
+type HorizontalFold = {
+  y: number;
+};
+
+type VerticalFold = {
+  x: number;
+};
+
+type Fold = HorizontalFold | VerticalFold;
+
+type PuzzleData = {
+  dots: Coord[];
+  folds: Fold[];
+};
+
+function parseInput(input: string[]) {
+  return input.reduce<PuzzleData>(
+    (puzzleData, line) => {
+      if (/\d+,\d+/.test(line)) {
+        puzzleData.dots.push(parseCoord(line));
+      }
+
+      if (/fold along/.test(line)) {
+        puzzleData.folds.push(parseFold(line));
+      }
+
+      return puzzleData;
+    },
+    {
+      dots: [],
+      folds: [],
+    }
+  );
+}
+
+function parseCoord(line: string): Coord {
+  const [x, y] = line.split(",").map(Number);
+  return { x, y };
+}
+
+function parseFold(line: string): Fold {
+  const [_, axis, lineNum] = line.match(/(x|y)=(\d+)/) || [];
+  return {
+    [axis]: Number(lineNum),
+  } as unknown as Fold;
 }
